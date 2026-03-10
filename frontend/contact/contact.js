53ae1afa8d3f22e21e25f03911b75c5696eba097
Cedrik-Meyer <cedrik.meyer02@gmail.com>
2026-03-10 17:09:36 +0100

    Script in contact.js ausgelagert

---
+++ frontend/contact/contact.js
@@ -0,0 +1,70 @@
+//Beim Laden der Seite wird die UserID des eingeloggten Benutzers in das versteckte Feld eingetragen, damit der Kontakt diesem Benutzer zugeordnet werden kann
+document.querySelector("form").addEventListener("submit", function() {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+    document.getElementById("userID").value = user.userID;
+});
+
+function addField() {
+    const container = document.getElementById('customFields');
+    const field = document.createElement('div');
+    const i = Date.now();
+
+    field.id = `${i}`;
+    field.style.marginBottom = '10px';
+    field.innerHTML = `
+                        <input type = "text" id = "label_${i}" name = "label_${i}" placeholder = "Bezeichnung"><br>
+                        <input type="text" id = "input_${i}" name="input_${i}" placeholder="Wert"><br>
+                        <button type = "button" onclick = "removeField(${i})">Feld löschen</button>`
+
+    container.appendChild(field);
+}
+
+function removeField(id) {
+    const field = document.getElementById(id);
+    field.remove();
+
+}
+
+window.onload = async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const contactId = urlParams.get('id');
+
+    if (!contactId) {
+        return;
+    }
+
+    document.getElementById('contactId').value = contactId;
+
+    try {
+        const response = await fetch(`/api/contacts/${contactId}`);
+        const contact = await response.json();
+
+        document.getElementById('firstName').value = contact.first_name || '';
+        document.getElementById('lastName').value = contact.last_name || '';
+        document.getElementById('email').value = contact.email || '';
+        document.getElementById('telephoneNumber').value = contact.telephone_number || '';
+
+        if (contact.custom_fields) {
+            for (const [key, value] of Object.entries(contact.custom_fields)) {
+                loadCustomField(key, value);
+            }
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+};
+
+function loadCustomField(labelName, inputValue) {
+    const container = document.getElementById('customFields');
+    const field = document.createElement('div');
+    const i = Date.now();
+
+    field.id = `field_${i}`;
+    field.style.marginBottom = '10px';
+    field.innerHTML = `
+                        <input type="text" id="label_${i}" name="label_${i}" value="${labelName}" placeholder="Bezeichnung"><br>
+                        <input type="text" id="input_${i}" name="input_${i}" value="${inputValue}" placeholder="Wert"><br>
+                        <button type="button" onclick="removeField('field_${i}')">Feld löschen</button>`;
+
+    container.appendChild(field);
+}
